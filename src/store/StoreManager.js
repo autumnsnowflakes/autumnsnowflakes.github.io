bdc4a74b6f334cd567e8c023ffacc185b02e6c14
siddharh <siddharthjn93@gmail.com>
2017-09-21 14:10:19 +0530

    wow ice

--- src/store/StoreManager.js
+++ src/store/StoreManager.js
@@ -2,13 +2,17 @@ const init = function () {
   let store = {};
   store.name = 'mithril';
   store.mode = 'landing'; // landing, project {id:}, resume, contact 
-
+  store.isSectionOpen = false;
   store.isMobile = function () {
     return window.innerHeight > window.innerWidth;
   };
   store.isTab = function () {
     return window.innerHeight > window.innerWidth;
   };
+
+  store.section=[
+   
+  ]
 
   return store;
 }
